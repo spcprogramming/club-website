b1077fc75119858bdeda810d1b5de64bc06dc183
jacobjg20 <grzetich20@gmail.com>
2019-01-02 15:03:54 -0800

    removed navigation and moved it to a header.php included header.php on all web pages removed Club Website folder and move all contents out

--- FAQ.php
+++ FAQ.php
@@ -27,26 +27,9 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #133e69; text-decoration-color: #00000;">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="http://localhost/club-website/Club%20Website/">SPC Programming Club</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="http://localhost/club-website/Club%20Website/">Home</a></li>
-            <li><a href="FAQ.php">FAQs</a></li>
-            <li><a href="contact.php">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+
+    <?php include 'partials/header.php';?>
+
     <main class="container">
         <div class="text-center">
             <h3>
